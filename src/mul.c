d9af8321c58835c3bdbbaf9e79d215ce8896d507
Dylan <86224129+xudarenbaba@users.noreply.github.com>
2025-07-28 22:28:23 +0800

    Update mul.c

--- src/mul.c
+++ src/mul.c
@@ -1,15 +1,31 @@
+// mul.c
 #include "mul.h"
 
-void mul(int msize, int tidx, int numt, Vector *vec, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM])
+void mul_kernel(int msize, int tidx, int numt,
+                TYPE (*restrict a)[NUM],
+                TYPE (*restrict bT)[NUM],
+                TYPE (*restrict c)[NUM])
 {
-    int i,j,k;
-
-    for (i = tidx; i < msize; i = i + numt) {
-        for (j = 0; j < msize; j++) {
-            for (k = 0; k < msize; k++) {
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+    for (int i = tidx; i < msize; i += numt) {
+        TYPE *restrict ci = c[i];
+        TYPE *restrict ai = a[i];
+        for (int k = 0; k < msize; k++) {
+            TYPE aik = ai[k];
+            TYPE *restrict bTk = bT[k];   // bT[k][j] == b[j][k]
+            for (int j = 0; j < msize; j++) {
+                ci[j] += aik * bTk[j];
             }
-            vector_append(vec, c[i][j]);
+        }
+    }
+}
+
+void transpose_mat(int msize,
+                   TYPE (*restrict src)[NUM],
+                   TYPE (*restrict dst)[NUM])
+{
+    for (int i = 0; i < msize; ++i) {
+        for (int j = 0; j < msize; ++j) {
+            dst[j][i] = src[i][j];
         }
     }
 }
