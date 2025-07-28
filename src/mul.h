8c8516a34fadf2a5cd1be13a138375d1f614737a
Dylan <86224129+xudarenbaba@users.noreply.github.com>
2025-07-28 22:28:51 +0800

    Update mul.h

--- src/mul.h
+++ src/mul.h
@@ -1,13 +1,37 @@
-#include "vector.h"
+// mul.h
+#ifndef MUL_H
+#define MUL_H
+
+#include <stddef.h>
 
 #define MAXTHREADS 16
-#define NUM 2048
+#define NUM        2048
 
 typedef double TYPE;
-typedef TYPE array[NUM];
 
-extern void mul(int msize, int tidx, int numt, Vector *vec, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM]);
+// 工具函数
+void Initialize(void);
+int  getCPUCount(void);
+double getCPUFreq(void);
 
-void Initialize();
-void ParallelMultiply(int msize, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM]);
-void GetModelParams(int *nthreads, int* msize, int print);
+// 计算相关
+void GetModelParams(int *nthreads, int *msize, int print);
+
+// bT 是 b 的转置
+void ParallelMultiply(int msize,
+                      TYPE (*restrict a)[NUM],
+                      TYPE (*restrict bT)[NUM],
+                      TYPE (*restrict c)[NUM]);
+
+// 线程内核
+void mul_kernel(int msize, int tidx, int numt,
+                TYPE (*restrict a)[NUM],
+                TYPE (*restrict bT)[NUM],
+                TYPE (*restrict c)[NUM]);
+
+// 转置
+void transpose_mat(int msize,
+                   TYPE (*restrict src)[NUM],
+                   TYPE (*restrict dst)[NUM]);
+
+#endif
